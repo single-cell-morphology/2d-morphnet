a7d5ad8004ad9f2cf454c5983cf2e44bc379df61
Hojae Lee <hojaelee@umich.edu>
2021-07-20 19:20:58 -0400

    Minor update

--- training/dataset.py
+++ training/dataset.py
@@ -20,7 +20,6 @@ except ImportError:
     pyspng = None
 
 #----------------------------------------------------------------------------
-
 class Dataset(torch.utils.data.Dataset):
     def __init__(self,
         name,                   # Name of the dataset.
@@ -228,7 +227,7 @@ class ImageFolderDataset(Dataset):
         if labels is None:
             return None
         labels = dict(labels)
-        labels = [labels[fname.replace('\\', '/')] for fname in self._image_fnames]
+        labels = [labels["00000/" + fname.replace('\\', '/')] for fname in self._image_fnames]
         labels = np.array(labels)
         labels = labels.astype({1: np.int64, 2: np.float32}[labels.ndim])
         return labels
